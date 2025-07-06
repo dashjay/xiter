fd4ebe546ecea40e73e8f360f48f1d31489ffb1c
dashjay <dashjwz@gmail.com>
2025-07-07 01:14:33 +0800

    test slice reverse

--- pkg/xiter/xiter_common_test.go
+++ pkg/xiter/xiter_common_test.go
@@ -17,4 +17,18 @@ func TestXIterCommon(t *testing.T) {
 		assert.Equal(t, _range(0, 1000), xiter.ToSliceSeq2Key(input))
 		assert.Equal(t, _range(0, 1000), xiter.ToSliceSeq2Value(input))
 	})
+
+	t.Run("from slice reverse", func(t *testing.T) {
+		res := xiter.ToSlice(xiter.FromSliceReverse(_range(0, 1000)))
+		for i := 0; i < 1000; i++ {
+			assert.Equal(t, 1000-i-1, res[i])
+		}
+	})
+
+	t.Run("reverse", func(t *testing.T) {
+		res := xiter.ToSlice(xiter.Reverse(xiter.FromSlice(_range(0, 1000))))
+		for i := 0; i < 1000; i++ {
+			assert.Equal(t, 1000-i-1, res[i])
+		}
+	})
 }
