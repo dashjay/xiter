ac0750d9dac91b20e8ffb1bd9555807173397e9e
DashJay <45532257+dashjay@users.noreply.github.com>
2025-08-15 15:24:51 +0800

    Add more union (#10)
    * write code with kimi-k2-0711-preview
    
    * remove wrong test codes
    
    * add more union

--- pkg/union/union.go
+++ pkg/union/union.go
@@ -10,3 +10,87 @@ type U3[T1, T2, T3 any] struct {
 	T2 T2
 	T3 T3
 }
+
+type U4[T1, T2, T3, T4 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+}
+
+type U5[T1, T2, T3, T4, T5 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+}
+
+type U6[T1, T2, T3, T4, T5, T6 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+	T6 T6
+}
+
+type U7[T1, T2, T3, T4, T5, T6, T7 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+	T6 T6
+	T7 T7
+}
+
+type U8[T1, T2, T3, T4, T5, T6, T7, T8 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+	T6 T6
+	T7 T7
+	T8 T8
+}
+
+type U9[T1, T2, T3, T4, T5, T6, T7, T8, T9 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+	T6 T6
+	T7 T7
+	T8 T8
+	T9 T9
+}
+
+type U10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+	T6 T6
+	T7 T7
+	T8 T8
+	T9 T9
+	T10 T10
+}
+
+type U11[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+	T6 T6
+	T7 T7
+	T8 T8
+	T9 T9
+	T10 T10
+	T11 T11
+}
