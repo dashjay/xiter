442f9dcc5cdb2ccc976a988ea3bf76dbcd52dedd
kevin.zhao <kevin.zhao@higogame.net>
2025-10-10 14:33:33 +0800

    init sync pool when create

--- pkg/xsync/sync_pool.go
+++ pkg/xsync/sync_pool.go
@@ -3,37 +3,25 @@ package xsync
 import "sync"
 
 type SyncPool[T any] struct {
-	New  func() T
 	pool sync.Pool
-	once sync.Once
 }
 
 // NewSyncPool creates a new SyncPool with specified init function
 func NewSyncPool[T any](new func() T) *SyncPool[T] {
 	sp := &SyncPool[T]{
-		New: new,
+		pool: sync.Pool{New: func() interface{} {
+			return new()
+		}},
 	}
-	sp.init()
 	return sp
-}
-
-// init initializes the SyncPool
-func (s *SyncPool[T]) init() {
-	s.once.Do(func() {
-		s.pool.New = func() interface{} {
-			return s.New()
-		}
-	})
 }
 
 // Get wraps sync.Pool.Get.
 func (s *SyncPool[T]) Get() T {
-	s.init()
 	return s.pool.Get().(T)
 }
 
 // Put wraps sync.Pool.Put.
 func (s *SyncPool[T]) Put(x T) {
-	s.init()
 	s.pool.Put(x)
 }
